a78504346e43de293540409ee82a7e552bbd4405
U-VHA10\OITCLESHAFFM0 <OITCLESHAFFM0@LAS-LT90159.v22.med.va.gov>
2021-10-19 16:17:08 -0400

    tweaking the tweak

--- AWS/leaf-ancil/bin/leaf-ancil.ts
+++ AWS/leaf-ancil/bin/leaf-ancil.ts
@@ -6,10 +6,10 @@ import { LeafAncilStack } from '../lib/leaf-ancil-stack';
 const app = new cdk.App();
 
 new LeafAncilStack(app, 'LeafAncilStack', {
-  env: { 
-    account: '456456143286', 
-    region: 'us-east-1',
-  },
+  // env: { 
+  //   // account: 'vaec-aws-gov-valeaf', 
+  //   region: 'us-gov-east-1',
+  // },
   tags: {
     "environment": "prod"
   }
