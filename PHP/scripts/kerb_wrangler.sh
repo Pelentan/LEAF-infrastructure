6598997daf58039378334b8f68c1b9f951f2641b
Shaffer0 <OITCLESHAFFM0@va.gov>
2021-11-10 17:12:55 -0500

    modified kerb_wrangler

--- PHP/scripts/kerb_wrangler.sh
+++ PHP/scripts/kerb_wrangler.sh
@@ -9,23 +9,32 @@ init_kerberos () {
     krb_login=$(echo ${KRB_USER} | tr [A-Z] [a-z])@$(echo ${KRB_DOMAIN} | tr [a-z] [A-Z])
     echo ${KRB_PASSWORD} | kinit ${krb_login}
     renew_until=$(klist | grep "renew until" | sed 's/renew until //')
-    let runix=$(date -d "$renew_until" +%s)-86400
+    let runix=$(date -d "$renew_until" +%s)-86400 
+    if [ 10 -gt $runix ]
+    then
+        echo "Ticket init failed"
+        sleep 30s
+        init_kerberos
+    else
+        echo "Initialized ticket" >> kerb_log.log
+    fi
 }
 
 renew_kerberos () {
     current_expire=$(klist | grep "krbtgt/VHA.MED.VA.GOV@VHA.MED.VA.GOV")
-    kinit -R -c $cache 
+    kinit -R -c $cache   
     new_expire=$(klist | grep "krbtgt/VHA.MED.VA.GOV@VHA.MED.VA.GOV")
     if [ "$current_expire" = "$new_expire" ]
     then
-        echo "***** Failed renew.  Attempting reinit" >> kerb_log.log
+        echo "***** Failed to get ticket.  Attempting reinit" >> kerb_log.log
+        sleep 30s
         init_kerberos
     else
-        echo "Renewed ticket"
+        echo "Renewed Ticket" >> kerb_log.log
     fi
 }
 
-set_cache_perms () { 
+set_things () { 
     # This is so the application that needs to use the link can access the cache file.   
     chown www-data:www-data $cache
     ls -lah $cache
@@ -34,23 +43,26 @@ set_cache_perms () {
 # init_kerberos
 # set_cache_perms
 runix=0
+current_expire="No ticket"
 while true; do
     tNow=$(date +%s)
+    echo "Starting Loop"
     echo "---------------------------------------------------" >> kerb_log.log
     echo "Checking time stamps:  runix: $runix  now: $tNow" >> kerb_log.log
     date >> kerb_log.log
     if [ $runix -gt $tNow ] 
     then
+        echo "Refresh"
         echo "Refreshing kerberos ticket" >> kerb_log.log
         renew_kerberos
     else
+        echo "Initialize"
         echo "Starting or reinitializing kerberos" >> kerb_log.log
         init_kerberos
     fi
-    echo "Complete" >> kerb_log.log
+    echo "Complete loop" 
     klist >> kerb_log.log
-    set_cache_perms
-    sleep 9.5h
+    set_things
+    #sleep 9.5h
+    sleep 30s
 done
-
-
